1ef85c4cfbee2e93f5ed88b54031e06fd6d86771
Luncert <2725115515@qq.com>
2020-01-18 20:53:41 +0800

    Remove Shell.g4

--- src/Terd.ts
+++ src/Terd.ts
@@ -5,48 +5,67 @@ import path from 'path';
 
 const PackageInfo = require('../package.json');
 
-type DataHandler = (data: Uint8Array) => void
+type DataHandler = (data: Uint8Array | string) => void
 
 class Terd {
 
     private cwd: string
+    private lastSucceed: boolean = true
+    private writeOutput: DataHandler
+
+    public getPrompt(): string {
+        let cwd = process.cwd().replace(/\\/g, '/')
+        return chalk.cyan(cwd) + (this.lastSucceed ? chalk.greenBright('>') : chalk.red('>'))
+    }
 
     /**
      * bind DataHandler to consume terminal outputs
      * @param handler DataHandler
      */
-    onData(handler: DataHandler) {
+    public onData(handler: DataHandler) {
+        this.writeOutput = handler
+    }
 
+    public init() {
+        this.writeOutput(this.getPrompt())
     }
 
     /**
      * submit script to Terd to process
      * @param script string
      */
-    process(script: string) {
-
+    public process(script: Buffer) {
+        this.lastSucceed = !this.lastSucceed
+        this.writeOutput(this.getPrompt())
     }
 }
 
 export class UserInterface {
 
+    private terd: Terd
+
     constructor() {
+        this.terd = new Terd()
         process.stdin.setEncoding('utf8')
     }
 
     start() {
-        process.stdin.on('data', (data) => {
-            this.write(this.getPrompt())
-        })
+        // output banner
+        this.write(this.loadBanner())
+
+        // watch output
+        this.terd.onData(this.write.bind(this))
+
+        // watch input
+        process.stdin.on('data', (data) => this.terd.process(data))
         
+        // capture SIGINT
         process.on('SIGINT', () => {
-            this.write('\x1b[0m\r\n')
+            this.write('\r\n')
             process.exit()
         })
 
-        this.write(this.loadBanner())
-        this.write('\r\n')
-        this.write(this.getPrompt(true))
+        this.terd.init()
     }
 
     loadBanner() {
@@ -55,19 +74,10 @@ export class UserInterface {
         lines[lines.length - 4] += chalk.redBright(PackageInfo.version)
         lines[lines.length - 3] += chalk.blueBright('by ' + PackageInfo.author)
         lines[lines.length - 2] += chalk.whiteBright(chalk.underline(PackageInfo.homepage))
-        return lines.join('\r\n')
-    }
-
-    getPrompt(lastSucceed?: boolean) {
-        let cwd = process.cwd()
-        let i = cwd.indexOf(':')
-        let disk = cwd.substring(0, i + 1)
-        let workPath = cwd.substring(i +  1).replace(/\\/g, '/')
-        return chalk.bgBlueBright(disk) + chalk.cyan(workPath) +
-            (lastSucceed ? chalk.greenBright('>') : chalk.red('>'))
+        return lines.join('\r\n') + '\r\n'
     }
 
     write(buffer: Uint8Array | string, cb?: (err?: Error) => void): boolean {
-        return process.stdout.write(buffer)
+        return process.stdout.write(buffer, cb)
     }
 }
