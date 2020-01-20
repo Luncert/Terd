975b785ac3fa02cfcd88b5edb32c63ef859df4f0
Luncert <2725115515@qq.com>
2020-01-20 15:27:46 +0800

    Bug: Run shell inside powershell.

--- src/Terd.ts
+++ src/Terd.ts
@@ -10,6 +10,8 @@ const PackageInfo = require('../package.json');
 type DataHandler = (data: Uint8Array | string) => void
 type CloseListener = () => void
 
+const TERD_MARKER = 'TERD#MARKER'
+
 class Terd {
 
     private cwd: string
@@ -23,21 +25,19 @@ class Terd {
         }
         this.writeOutput = output
 
-        this.proc = child_process.spawn('powershell', {
-            argv0: '-Command -',
-            shell: false,
-        })
-        this.proc.stdout.on('data', (data) => this.writeOutput(iconv.decode(data, 'gbk')))
+        this.cwd = process.cwd().replace(/\\/g, '/')
+        this.proc = child_process.spawn('powershell -NoLogo -Command -', { shell: true })
+        this.proc.stdout.on('data', this.processOutput.bind(this))
         this.proc.on('close', () => onClose && onClose())
 
         process.on('SIGINT', () => this.kill())
 
-        this.writeOutput(this.getPrompt())
+        this.writeOutput(Buffer.from(this.getPrompt()))
     }
 
-    public getPrompt(): string {
-        let cwd = process.cwd().replace(/\\/g, '/')
-        return chalk.cyan(cwd) + (this.lastSucceed ? chalk.greenBright('>') : chalk.red('>'))
+    public getPrompt(): number[] {
+        let str = chalk.cyan(this.cwd) + (this.lastSucceed ? chalk.greenBright('>') : chalk.red('>'))
+        return str.split('').map((c) => c.charCodeAt(0))
     }
 
     /**
@@ -46,6 +46,35 @@ class Terd {
      */
     public process(script: Buffer) {
         this.proc.stdin.write(script)
+        this.proc.stdin.write(';echo ' + TERD_MARKER + '${pwd};\r\n')
+    }
+
+    private processOutput(raw: Buffer) {
+        let data = new Array<number>(...raw)
+        let markStart = -1
+        for (let i = 0; i < data.length; i++) {
+            let c = data[i]
+            if (c == TERD_MARKER.charCodeAt(0)) {
+                markStart = i
+            } else if (markStart > -1) {
+                let cursor = i - markStart
+                if (cursor == TERD_MARKER.length) {
+                    for (let j = i; j < data.length; j++) {
+                        if (data[j] == 13) { // '\n'
+                            this.cwd = String.fromCharCode(...data.slice(i, j)).replace(/\\/g, '/') // j - 1 = '\r'
+                            let prompt = this.getPrompt()
+                            i += prompt.length
+                            data.splice(markStart, j + 2 - markStart, ...prompt) // 2 = '\r\n', which has already been included by promt
+                            // console.log('<', data.length, 0, j - markStart, String.fromCharCode(...data))
+                        }
+                    }
+                    markStart = -1
+                } else if (c != TERD_MARKER.charCodeAt(cursor)) {
+                    markStart = -1
+                }
+            }
+        }
+        this.writeOutput(iconv.decode(Buffer.from(data), 'gbk'))
     }
 
     public kill() {
@@ -77,7 +106,7 @@ export class UserInterface {
         let lines = fs.readFileSync(path.resolve(__dirname, 'banner.txt')).toString().split('\r\n')
             .map((line) => chalk.greenBright(line))
         // lines[lines.length - 4] += chalk.redBright(PackageInfo.version)
-        lines[lines.length - 4] += chalk.redBright(PackageInfo.version)
+        lines[lines.length - 4] += chalk.redBright('v' + PackageInfo.version)
         lines[lines.length - 3] += chalk.blueBright('by ' + PackageInfo.author)
         lines[lines.length - 2] += chalk.whiteBright(chalk.underline(PackageInfo.homepage))
         return lines.join('\r\n') + '\r\n'
